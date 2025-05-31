3f54e0de984d2ebfc590393c15443f58553da56a
Kanish <kanishkmalviya78@gmail.com>
2025-05-31 15:35:44 +0530

    removed stale routes used for testing while project setup

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -10,39 +10,6 @@ import json
 router = APIRouter()
 router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 
-@router.get("/pingwithouterror", response_model=SuccessResponse)
-async def ping():
-    try:
-        # Simulating normal working
-        data = "pong"
-        return SuccessResponse(status=True, data=data)
-    except Exception as e:
-        # Catching unexpected errors and raising properly
-        print("excep ", e)
-        raise APIException(detail = str(e) or "Failed to ping the service.", status_code = 500)
-    
-@router.get("/pingwitherror", response_model=SuccessResponse)
-async def ping():
-    try:
-        # Force an exception (for testing)
-        raise ValueError("Simulated error")
-        # Simulating normal working
-        
-        data = "pong"
-        return SuccessResponse(status=True, data=data)
-    except Exception as e:
-        # Catching unexpected errors and raising properly
-        print("excep ", e)
-        raise APIException(detail = str(e) or "Failed to ping the service.", status_code = 500)
-
-@router.get("/me", response_model=SuccessResponse)
-async def get_me(current_user: dict = Depends(get_authenticated_user)):
-    try:
-        return SuccessResponse(status=True, data={"user": current_user},message="You are authenticated")
-    except Exception as e:
-        print('exp ', str(e))
-    raise APIException(detail = str(e) or "Failed to me the service.", status_code = 500)
-
 @router.get("/ingest", response_model=SuccessResponse)
 async def ingest(current_user: dict = Depends(get_authenticated_user)):
     try:
--- app/main.py
+++ app/main.py
@@ -16,9 +16,6 @@ logger = setup_logging()
 
 app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG)
 
-# print("settings",settings.APP_NAME)
-# print("settings", settings.DEBUG)
-
 # Register exception handlers globally
 app.add_exception_handler(APIException, api_exception_handler)
 app.add_exception_handler(HTTPException, http_exception_handler)
@@ -27,7 +24,7 @@ app.include_router(v1_router, prefix="/api/v1")
 
 @app.get("/", response_model=SuccessResponse)
 async def root():
-    print("-----------------------------------------------Hello from root!")
+    # This is for testing root route
     logger.info("Root endpoint accessed")
     return SuccessResponse(status=True, data=f"Welcome to {settings.APP_NAME}!")
 
@@ -35,10 +32,3 @@ async def root():
 async def trigger_error():
     # This is to manually trigger an error for testing
     raise APIException("Something went wrong!", status_code=500)
-
-@app.get("/item/{item_id}", response_model=SuccessResponse)
-async def get_item(item_id: int):
-    # Simulate checking if the item exists
-    if item_id != 1:
-        raise APIException("Item not found!", status_code=404)
-    return SuccessResponse(status=True, data={"item_id": item_id, "name": "Item 1"})
